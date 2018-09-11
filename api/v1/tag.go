c8b0ba3093209506f64fba3f015da463ff7da0f2
muti <261809340@qq.com>
2018-09-11 19:03:42 +0800

    fix database error
    Signed-off-by: muti <261809340@qq.com>

--- api/v1/tag.go
+++ api/v1/tag.go
@@ -3,35 +3,20 @@ package v1
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/muti/models"
-	"github.com/muti/pkg/app"
-	"github.com/muti/pkg/errno"
-	"gopkg.in/mgo.v2/bson"
-	"net/http"
 )
 
 var (
 	dao = models.Tags{}
 )
 
-func GetTags(c *gin.Context) (r http.Response) {
-	defer r.Body.Close()
-	appG := app.Gin{C: c}
-	var tags []models.Tags
-	tags, err := dao.FindAllTags()
-	if err != nil {
-		appG.Response(http.StatusOK, errno.ERROR_GET_TAGS_FAIL, nil)
-		return
-	}
-	appG.Response(http.StatusOK, errno.SUCCESS, tags)
-	return
+func GetTags(c *gin.Context) {
+
 }
 
 func GetTag(c *gin.Context) {
-	id := bson.ObjectId.String("_id")
-	appG := app.Gin{C: c}
-	result, err := dao.FindTagById(id)
-	if err != nil {
-		appG.Response(http.StatusOK, errno.ERROR_NOT_EXIST_TAG, nil)
-	}
-	appG.Response(http.StatusOK, errno.SUCCESS, result)
+
+}
+
+func AddTag(c *gin.Context) {
+
 }
--- models/db.go
+++ models/db.go
@@ -10,10 +10,12 @@ var globalS *mgo.Session
 
 func InitData() {
 	dialInfo := &mgo.DialInfo{
-		Addrs:    []string{viper.GetString("database.DBHost")},
-		Source:   viper.GetString("database.Source"),
-		Username: viper.GetString("database.User"),
-		Password: viper.GetString("database.DBPass"),
+		Addrs:     []string{viper.GetString("database.DBHost")},
+		Source:    viper.GetString("database.AuthDb"),
+		Username:  viper.GetString("database.AuthUser"),
+		Password:  viper.GetString("database.AuthPass"),
+		Timeout:   viper.GetDuration("database.TimeOut"),
+		PoolLimit: viper.GetInt("database.PoolLimit"),
 	}
 
 	s, err := mgo.DialWithInfo(dialInfo)
@@ -28,15 +30,40 @@ func InitData() {
 //每一次操作都copy一份 Session,避免每次创建Session,导致连接数量超过设置的最大值
 //获取文档对象 c := Session.DB(db).C(collection)
 func connect(db, collection string) (*mgo.Session, *mgo.Collection) {
-	s := globalS.Copy()
-	c := s.DB(db).C(collection)
-	return s, c
+	ms := globalS.Copy()
+	c := ms.DB(db).C(collection)
+	ms.SetMode(mgo.Monotonic, true)
+	return ms, c
+}
+
+func getDb(db string) (*mgo.Session, *mgo.Database) {
+	ms := globalS.Copy()
+	return ms, ms.DB(db)
+}
+
+//Document是否为空
+func IsEmpty(db, collection string) bool {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	count, err := c.Count()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	return count == 0
+}
+
+//查询满足条件的数量
+func Count(db, collection string, query interface{}) (int, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Find(query).Count()
 }
 
 //插入操作
 func Insert(db, collection string, docs ...interface{}) error {
 	ms, c := connect(db, collection)
 	defer ms.Close()
+
 	return c.Insert(docs...)
 }
 
@@ -48,30 +75,221 @@ func IsExist(db, collection string, query interface{}) bool {
 	return count > 0
 }
 
-//查询单个对象
+//查找单个对象
 func FindOne(db, collection string, query, selector, result interface{}) error {
 	ms, c := connect(db, collection)
 	defer ms.Close()
+
 	return c.Find(query).Select(selector).One(result)
 }
 
-//查询所有对象
+//查找所有对象
 func FindAll(db, collection string, query, selector, result interface{}) error {
 	ms, c := connect(db, collection)
 	defer ms.Close()
+
 	return c.Find(query).Select(selector).All(result)
 }
 
-//更新操作
-func Update(db, collection string, query, update interface{}) error {
+//对查询结果分页处理
+func FindPage(db, collection string, page, limit int, query, selector, result interface{}) error {
 	ms, c := connect(db, collection)
 	defer ms.Close()
-	return c.Update(query, update)
+
+	return c.Find(query).Select(selector).Skip(page * limit).Limit(limit).All(result)
 }
 
-//删除操作
+//查询满足条件的指针
+func FindIter(db, collection string, query interface{}) *mgo.Iter {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	return c.Find(query).Iter()
+}
+
+//更新满足条件的一个document
+func Update(db, collection string, selector, update interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	return c.Update(selector, update)
+}
+
+//更新，如果不存在就插入一个新的document
+func Upsert(db, collection string, selector, update interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	_, err := c.Upsert(selector, update)
+	return err
+}
+
+//更新满足条件的所有的docuement
+func UpdateAll(db, collection string, selector, update interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	_, err := c.UpdateAll(selector, update)
+	return err
+}
+
+//删除满足条件的一个 document
 func Remove(db, collection string, query interface{}) error {
 	ms, c := connect(db, collection)
 	defer ms.Close()
+
 	return c.Remove(query)
+}
+
+//删除满足条件的所有 document
+func RemoveAll(db, collection string, selector interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	_, err := c.RemoveAll(selector)
+	return err
+}
+
+//批量的插入操作
+func BulkInsert(db, collection string, docs ...interface{}) (*mgo.BulkResult, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	bulk := c.Bulk()
+	bulk.Insert(docs...)
+	return bulk.Run()
+}
+
+//批量删除操作
+func BulkRemove(db, collection string, selector ...interface{}) (*mgo.BulkResult, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	bulk := c.Bulk()
+	bulk.Remove(selector...)
+	return bulk.Run()
+}
+
+//批量删除所有
+func BulkRemoveAll(db, collection string, selector ...interface{}) (*mgo.BulkResult, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	bulk := c.Bulk()
+	bulk.RemoveAll(selector...)
+	return bulk.Run()
+}
+
+//批量更新
+func BulkUpdate(db, collection string, pairs ...interface{}) (*mgo.BulkResult, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	bulk := c.Bulk()
+	bulk.Update(pairs...)
+	return bulk.Run()
+}
+
+//批量更新所有
+func BulkUpdateAll(db, collection string, pairs ...interface{}) (*mgo.BulkResult, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	bulk := c.Bulk()
+	bulk.UpdateAll(pairs...)
+	return bulk.Run()
+}
+
+//批量更新，如果不存在就插入一个新的document
+func BulkUpsert(db, collection string, pairs ...interface{}) (*mgo.BulkResult, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	bulk := c.Bulk()
+	bulk.Upsert(pairs...)
+	return bulk.Run()
+}
+
+//聚合操作查找所有
+func PipeAll(db, collection string, pipeline, result interface{}, allowDiskUse bool) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	var pipe *mgo.Pipe
+	if allowDiskUse {
+		pipe = c.Pipe(pipeline).AllowDiskUse()
+	} else {
+		pipe = c.Pipe(pipeline)
+	}
+	return pipe.All(result)
+}
+
+//聚合操作查找一个
+func PipeOne(db, collection string, pipeline, result interface{}, allowDiskUse bool) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	var pipe *mgo.Pipe
+	if allowDiskUse {
+		pipe = c.Pipe(pipeline).AllowDiskUse()
+	} else {
+		pipe = c.Pipe(pipeline)
+	}
+	return pipe.One(result)
+}
+
+//聚合操作查找cursor(指针)
+func PipeIter(db, collection string, pipeline interface{}, allowDiskUse bool) *mgo.Iter {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	var pipe *mgo.Pipe
+	if allowDiskUse {
+		pipe = c.Pipe(pipeline).AllowDiskUse()
+	} else {
+		pipe = c.Pipe(pipeline)
+	}
+
+	return pipe.Iter()
+
+}
+
+//解释
+func Explain(db, collection string, pipeline, result interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	pipe := c.Pipe(pipeline)
+	return pipe.Explain(result)
+}
+
+//大文件创建
+func GridFSCreate(db, prefix, name string) (*mgo.GridFile, error) {
+	ms, d := getDb(db)
+	defer ms.Close()
+	gridFs := d.GridFS(prefix)
+	return gridFs.Create(name)
+}
+
+//查找单个大文件
+func GridFSFindOne(db, prefix string, query, result interface{}) error {
+	ms, d := getDb(db)
+	defer ms.Close()
+	gridFs := d.GridFS(prefix)
+	return gridFs.Find(query).One(result)
+}
+
+//查找所有大文件
+func GridFSFindAll(db, prefix string, query, result interface{}) error {
+	ms, d := getDb(db)
+	defer ms.Close()
+	gridFs := d.GridFS(prefix)
+	return gridFs.Find(query).All(result)
+}
+
+//大文件开放
+func GridFSOpen(db, prefix, name string) (*mgo.GridFile, error) {
+	ms, d := getDb(db)
+	defer ms.Close()
+	gridFs := d.GridFS(prefix)
+	return gridFs.Open(name)
+}
+
+//大文件删除
+func GridFSRemove(db, prefix, name string) error {
+	ms, d := getDb(db)
+	defer ms.Close()
+	gridFs := d.GridFS(prefix)
+	return gridFs.Remove(name)
 }
